c629dc73beab461f6d48d37cec1d99df4cc17448
Zorua <milandehaart@live.nl>
2023-11-19 23:07:09 +0100

    added slider component

---
+++ src/lib/components/shared/card/slider.svelte
@@ -0,0 +1,26 @@
+<script lang=ts>
+    export let link: string = "/"
+    export let classList: string = "";
+    export let img: string = "";
+</script>
+
+<a 
+    href={link}
+    class="
+        flex-shrink-0
+        snap-start
+        {classList}
+">
+    {#if img}
+        <img 
+            src={img} 
+            alt="card"
+            class="
+                h-[18.75rem] 
+                w-[15.625rem] 
+                object-cover 
+                rounded-xl
+        ">
+    {/if}
+    <slot></slot>
+</a>
