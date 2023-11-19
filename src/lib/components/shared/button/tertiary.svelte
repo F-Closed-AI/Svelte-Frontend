0d275e236327d04c2b7b82220c3c12a8d95066b4
Zorua <milandehaart@live.nl>
2023-11-19 23:06:47 +0100

    added tertiary button component

---
+++ src/lib/components/shared/button/tertiary.svelte
@@ -0,0 +1,26 @@
+<script lang=ts>
+    export let link: string = "";    
+    export let classList: string = "";
+    export let iconPosition: "left" | "right" | "up" | "down" = "right";
+
+    let baseClasses: string = `items-center gap-5 text-light-text-primary bg-light-btn-primary-color rounded-lg px-14 py-3 w-max ${iconPosition === "left" && "flex"} ${iconPosition === "right" && "flex flex-row-reverse"} ${iconPosition === "down" && "flex flex-col-reverse"}`
+</script>
+
+{#if link != ""}
+    <a 
+        href={link}
+        class="
+            {baseClasses}
+            {classList}
+    ">
+        <slot/>
+    </a>
+{:else}
+    <button
+        class="
+            {baseClasses}
+            {classList}
+    ">
+        <slot/>     
+    </button>
+{/if}
