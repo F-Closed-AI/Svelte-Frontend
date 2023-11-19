9bec5ec214187ecbfd86de4f2d4ee64053e600f0
Zorua <milandehaart@live.nl>
2023-11-19 23:08:14 +0100

    added small text component

---
+++ src/lib/components/shared/text/sm.svelte
@@ -0,0 +1,16 @@
+<script lang=ts>
+    export let skeleton: boolean = false;
+    export let classList: string = ""
+</script>
+
+<span 
+    class:skeleton={skeleton} 
+    class="
+        text-sm 
+        font-normal 
+        max-w-max
+        {classList}
+    "
+>
+    <slot></slot>
+</span>
