96607ea2ee89e3823778e5957c3cc7ca44b28f25
Zorua <milandehaart@live.nl>
2023-11-19 23:08:23 +0100

    added extra large text component

---
+++ src/lib/components/shared/text/xl.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+    export let skeleton: boolean = false;
+    export let classList: string = "";
+</script>
+
+<h1 
+    class:skeleton={skeleton} 
+    class="
+        text-xl 
+        font-bold 
+        max-w-max
+        {classList}
+">
+    <slot></slot>
+</h1>
