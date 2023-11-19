4e0523259678db5764638c2a7eee94d4535e906e
Zorua <milandehaart@live.nl>
2023-11-19 23:07:49 +0100

    added base text component

---
+++ src/lib/components/shared/text/base.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+    export let skeleton: boolean = false;
+    export let classList: string = "";
+</script>
+
+<span 
+    class:skeleton={skeleton} 
+    class="
+        font-normal 
+        text-base 
+        max-w-max
+        {classList}
+">
+    <slot></slot>
+</span>
