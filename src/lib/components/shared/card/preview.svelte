e564096550e5fe9ee5fd34641acfcad4233af6e7
Zorua <milandehaart@live.nl>
2023-11-19 23:07:04 +0100

    added preview card component

---
+++ src/lib/components/shared/card/preview.svelte
@@ -0,0 +1,37 @@
+<script lang=ts>
+    export let classList: string = "";
+    export let img: string = "";
+</script>
+
+<div class="
+    flex
+    flex-col
+    gap-8
+">
+    <div class="
+        flex
+        flex-col
+        gap-2
+    ">
+        {#if img}
+            <img 
+                src={img} 
+                alt="card"
+                class="
+                    w-full
+                    !h-[10rem]  
+                    object-cover
+                    rounded-xl
+                    {classList}
+            ">
+        {/if}
+        <slot name="name"></slot>
+    </div>
+    <div class="
+        flex
+        gap-32
+    ">
+        <slot></slot>
+    </div>
+    <slot name="button"></slot>
+</div>
