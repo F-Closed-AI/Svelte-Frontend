96bfe4e91d70588e441e3cfcedb527d97e0c66e5
Zorua <milandehaart@live.nl>
2023-11-19 23:08:01 +0100

    added detail text component

---
+++ src/lib/components/shared/text/detail.svelte
@@ -0,0 +1,20 @@
+<script lang=ts>
+    export let classList: string = "";
+</script>
+
+<div class="
+    flex
+    flex-col
+    items-center
+    w-max
+    {classList}
+">
+    <div class="
+        flex
+        items-baseline
+    ">
+        <slot name="amount"></slot>
+        <slot name="format"></slot>
+    </div>
+    <slot></slot>
+</div>
