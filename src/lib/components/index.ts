9ab7a9824df3357b642f7606b9ace5cf89372fee
Zorua <milandehaart@live.nl>
2023-11-19 23:05:53 +0100

    Added component barrel file

---
+++ src/lib/components/index.ts
@@ -0,0 +1,53 @@
+/* Layout */
+
+
+export { default as Header } from "../components/layout/header.svelte";
+export { default as Sidebar } from "../components/layout/sidebar.svelte";
+export { default as Wrapper } from "../components/layout/wrapper.svelte";
+
+/* Shared */
+
+
+// Button 
+export { default as PrimaryButton } from "../components/shared/button/primary.svelte";
+export { default as SecondaryButton } from "../components/shared/button/secondary.svelte";
+export { default as TertiaryButton } from "../components/shared/button/tertiary.svelte";
+
+// Card
+export { default as BannerCard } from "../components/shared/card/banner.svelte";
+export { default as NewCard } from "../components/shared/card/new.svelte";
+export { default as PreviewCard } from "../components/shared/card/preview.svelte";
+export { default as SliderCard } from "../components/shared/card/slider.svelte";
+
+// Input
+export { default as Input } from "../components/shared/input/input.svelte";
+
+// Object
+export { default as Line } from "../components/shared/object/line.svelte";
+export { default as Tooltip } from "../components/shared/object/tooltip.svelte";
+
+// Other
+export { default as Logo } from "../components/shared/other/logo.svelte";
+export { default as Slider } from "../components/shared/other/slider.svelte";
+
+// Text
+export { default as TextBase } from "../components/shared/text/base.svelte";
+export { default as TextDecoration } from "../components/shared/text/decoration.svelte";
+export { default as TextDetail } from "../components/shared/text/detail.svelte";
+export { default as TextLarge } from "../components/shared/text/lg.svelte";
+export { default as TextSmall } from "../components/shared/text/sm.svelte";
+export { default as TextXL } from "../components/shared/text/xl.svelte";
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
