018d1939edc34b5decab7207b0a0d3ddfbc89f70
Zorua <milandehaart@live.nl>
2023-11-19 23:08:09 +0100

    added large text component

---
+++ src/lib/components/shared/text/lg.svelte
@@ -0,0 +1,13 @@
+<script lang=ts>
+    export let skeleton: boolean = false;
+</script>
+
+<h2 
+    class:skeleton={skeleton} 
+    class="
+        text-lg 
+        font-semibold 
+        max-w-max
+">
+    <slot></slot>
+</h2>
