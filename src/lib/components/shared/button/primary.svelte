3710d5d0de8bfaabbdf586901b1c90efd2783622
Zorua <milandehaart@live.nl>
2023-11-19 23:06:29 +0100

    added primary button component

---
+++ src/lib/components/shared/button/primary.svelte
@@ -0,0 +1,26 @@
+<script lang=ts>
+    export let link: string = "";    
+    export let classList: string = "";
+
+    let baseClasses: string = "bg-light-btn-primary-background text-light-text-tertiary w-max py-4 px-8 rounded-xl transition-opacity hover:opacity-90"
+</script>
+
+{#if link != ""}
+    <a 
+        href={link}
+        class="
+            w-max
+            {baseClasses}
+            {classList}
+    ">
+        <slot/>
+    </a>
+{:else}
+    <button
+        class="
+            {baseClasses}
+            {classList}
+    ">
+        <slot/>     
+    </button>
+{/if}
