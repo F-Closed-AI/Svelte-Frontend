50ac189007c3a06243e1dd7d606d8acd1b6231ac
Zorua <milandehaart@live.nl>
2023-11-19 23:06:58 +0100

    added new card component

---
+++ src/lib/components/shared/card/new.svelte
@@ -0,0 +1,28 @@
+<script lang=ts>
+    export let classList: string = "";
+    export let img: string = "";
+</script>
+
+<button class="
+    flex 
+    gap-3 
+    items-center
+">
+    <img 
+        src={img} 
+        alt="card"
+        class="
+            w-[6.25rem]
+            h-[6.25rem]
+            object-cover
+            rounded-xl
+            {classList}
+        "
+    >
+    <div class="
+        flex
+        flex-col
+    ">
+        <slot></slot>
+    </div>
+</button>
