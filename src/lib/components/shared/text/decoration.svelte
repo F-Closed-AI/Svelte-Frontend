3b096b5d749250f2a331048f866cb8d6f2851006
Zorua <milandehaart@live.nl>
2023-11-19 23:07:56 +0100

    added decoration text component

---
+++ src/lib/components/shared/text/decoration.svelte
@@ -0,0 +1,29 @@
+<script lang=ts>
+    export let skeleton: boolean = false;
+    export let classList: string = "";
+    export let line: boolean = true;
+    export let link: string = "";
+
+    let baseClasses: string = `flex items-center gap-3 font-secondary font-normal text-base text-light-background max-w-max uppercase ${link && "link"} ${line && "before:inline-block before:content-[''] before:bg-light-background before:w-14 before:h-line"}`
+</script>
+
+{#if link != ""}
+    <a 
+        href="/"
+        class:skeleton={skeleton} 
+        class="   
+            {baseClasses}
+            {classList}
+    ">
+        <slot></slot>
+    </a>
+{:else}
+    <span 
+        class:skeleton={skeleton} 
+        class="   
+            {baseClasses}
+            {classList}
+    ">
+        <slot></slot>
+    </span>
+{/if}
