b1b60908726b82af1f744c6ba536140af1968a2a
Zorua <milandehaart@live.nl>
2023-11-21 23:03:49 +0100

    Added schemas barrel file

---
+++ src/lib/schemas/index.ts
@@ -0,0 +1,6 @@
+/* Schemas */
+
+
+// Character
+export { schemaCreateCharacter, type SchemaCreateCharacter, } from "./character/createCharacter"; 
+export { schemaSaveCharacter, type SchemaSaveCharacter, } from "./character/saveCharacter"; 
