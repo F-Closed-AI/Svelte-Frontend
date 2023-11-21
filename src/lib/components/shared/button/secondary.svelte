200428358b7a8ca8910010af247973d9d1b86cce
Zorua <milandehaart@live.nl>
2023-11-21 23:01:36 +0100

    Added secondary button component

---
+++ src/lib/components/shared/button/secondary.svelte
@@ -0,0 +1,30 @@
+<script lang=ts>
+    export let link: string = "";    
+    export let classList: string = "";
+    export let disabled: boolean = false;
+    export let onClick: (event: Event) => void = () => {};
+
+    let baseClasses: string = "flex items-center justify-center bg-light-btn-secondary-background text-light-text-primary w-max py-4 px-8 rounded-xl border-2 border-light-btn-primary-background transition-opacity hover:opacity-90"
+</script>
+
+{#if link != ""}
+    <a 
+        href={link}
+        class="
+            {baseClasses}
+            {classList}
+    ">
+        <slot/>
+    </a>
+{:else}
+    <button 
+        class="
+            {baseClasses}
+            {classList}
+        "
+        on:click={onClick}
+        disabled={disabled}
+    >
+        <slot/>     
+    </button>
+{/if}
