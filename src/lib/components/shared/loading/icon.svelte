93e305d212eb9fe6bf48c0c644a7c84707e8cb74
Zorua <milandehaart@live.nl>
2023-11-21 23:02:49 +0100

    Added loading icon component

---
+++ src/lib/components/shared/loading/icon.svelte
@@ -0,0 +1,6 @@
+<script lang=ts>
+    export let icon: string = "";
+    export let duration: string = "15s";
+</script>
+
+<i class="{icon} fa-spin" style="--fa-animation-duration: {duration}"></i>
