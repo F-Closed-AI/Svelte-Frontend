9f90fa6809b9f449ea2b07895ac2f43bfe275be2
Zorua <milandehaart@live.nl>
2023-11-21 23:00:48 +0100

    Added tab navigation item component

---
+++ src/lib/components/layout/tabNavigationItem.svelte
@@ -0,0 +1,17 @@
+<script lang=ts>
+    export let link: string = ""
+    export let active: boolean = false;
+</script>
+
+<a 
+    href={link}
+    class="
+        text-light-text-secondary
+        transition-colors
+        duration-300
+        hover:text-light-text-accent
+        {active && "text-light-text-accent"}
+    "
+>
+    <slot></slot>
+</a>
