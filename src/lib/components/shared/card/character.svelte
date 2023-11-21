77116b6798f8e224d30501af2ea3e9369fd17f5c
Zorua <milandehaart@live.nl>
2023-11-21 23:02:02 +0100

    Added character card component

---
+++ src/lib/components/shared/card/character.svelte
@@ -0,0 +1,47 @@
+<script lang=ts>
+    export let link: string = ""
+    export let classList: string = "";
+    export let imgClassList: string = "";
+    export let img: string = "";
+    export let showType: "grid" | "lines" | "slider" = "slider";
+</script>
+
+<a 
+    href={link}
+    class="
+        flex
+        gap-10
+        flex-shrink-0
+        {showType === "grid" && "xs:flex-grow"}
+        {showType === "lines" && "w-full sm:flex-col sm:bg-light-dashboard-dark sm:gap-0 sm:rounded-3xl"}
+        {classList}
+">
+    {#if img}
+        <img 
+            src={img} 
+            alt="card"
+            class="
+                flex-1
+                h-[18.75rem] 
+                w-[15.625rem] 
+                object-cover 
+                rounded-xl
+                {showType === "grid" && "w-full max-h-[40rem]"}
+                {showType === "lines" && "w-full object-scale-down bg-light-dashboard-dark sm:object-cover sm:flex-[0.5] sm:max-h-[10.25rem]"}
+                {imgClassList}
+        ">
+    {/if}
+    {#if showType === "lines"}
+        <div class="
+            flex-1
+            flex
+            flex-col
+            gap-6
+            justify-between
+            sm:justify-normal
+            sm:p-5
+        ">
+            <slot></slot>
+        </div>
+    {/if}
+</a>
