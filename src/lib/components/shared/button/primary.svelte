aa895f28ee2c806458a3e11de34927941f498c97
Zorua <milandehaart@live.nl>
2023-11-21 23:01:28 +0100

    Updated primary button component

--- src/lib/components/shared/button/primary.svelte
+++ src/lib/components/shared/button/primary.svelte
@@ -1,15 +1,16 @@
 <script lang=ts>
     export let link: string = "";    
     export let classList: string = "";
+    export let disabled: boolean = false;
+    export let onClick: (event: Event) => void = () => {};
 
-    let baseClasses: string = "bg-light-btn-primary-background text-light-text-tertiary w-max py-4 px-8 rounded-xl transition-opacity hover:opacity-90"
+    let baseClasses: string = "flex items-center justify-center bg-light-btn-primary-background text-light-text-tertiary w-max py-4 px-8 rounded-xl transition-opacity hover:opacity-90"
 </script>
 
 {#if link != ""}
     <a 
         href={link}
         class="
-            w-max
             {baseClasses}
             {classList}
     ">
@@ -20,7 +21,10 @@
         class="
             {baseClasses}
             {classList}
-    ">
+        "
+        on:click={onClick}
+        disabled={disabled}
+    >
         <slot/>     
     </button>
 {/if}
