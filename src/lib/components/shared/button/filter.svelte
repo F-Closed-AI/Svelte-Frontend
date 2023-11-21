83fd5f5a1123e3ce78af02f77556712c76bcc3eb
Zorua <milandehaart@live.nl>
2023-11-21 23:01:20 +0100

    Added filter button component

---
+++ src/lib/components/shared/button/filter.svelte
@@ -0,0 +1,22 @@
+<script lang=ts>
+    import { TextBase } from "$lib/components";
+
+    export let toggle: boolean = false
+</script>
+
+<button
+    on:click={() => toggle = !toggle}
+>
+    <TextBase>Sort by</TextBase>
+    <TextBase classList="
+        font-semibold
+        text-light-btn-primary-background
+    ">
+        <slot></slot>
+        <i 
+            class="fa-solid fa-chevron-down"
+            class:fa-chevron-down={!toggle}
+            class:fa-chevron-up={toggle}
+        ></i>
+    </TextBase>
+</button>
