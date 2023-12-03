6fd11279591de3e4264fddb93e604c78917f39d6
Zorua <milandehaart@live.nl>
2023-12-03 22:34:42 +0100

    Added the load animation wrapper component.

---
+++ src/lib/components/layout/loadAnimationWrapper.svelte
@@ -0,0 +1,11 @@
+<script lang=ts>
+    import { onMount } from "svelte";
+
+    let loaded: boolean = false;
+
+    onMount(() => loaded = true);
+</script>
+
+{#if loaded}
+    <slot></slot>
+{/if}
