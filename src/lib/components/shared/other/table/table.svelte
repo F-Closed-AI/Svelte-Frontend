59b2af5397f25d208b13fa4bf0fc305f4dda27c8
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:57:16 +0100

    Added table component

---
+++ src/lib/components/shared/other/table/table.svelte
@@ -0,0 +1,29 @@
+<script lang=ts>
+	import { TextSmall } from "$lib/components";
+
+    export let classList: string = "";
+    export let columns: string[] = [];
+</script>
+
+<table 
+    class="
+        w-full
+        text-left
+        {classList}
+">
+    <thead 
+        class="
+            lg:hidden
+    ">
+        <tr>
+            {#each columns as column}
+                <th>
+                    <TextSmall>{column}</TextSmall>
+                </th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        <slot></slot>
+    </tbody>
+</table>
