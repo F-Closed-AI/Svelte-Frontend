c3861cd41001e56a4da7322e58972a69b7edd45d
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:55:57 +0100

    Added circle button component

---
+++ src/lib/components/shared/button/circle.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let link: string = "";    
+    export let classList: string = "";
+    export let disabled: boolean = false;
+    export let onClick: (event: Event) => void = () => {};    
+
+    let baseClasses: string = "flex text-light-text-primary bg-light-background px-3 py-2 rounded-full cursor-pointer";
+</script>
+
+{#if link != ""}
+    <a 
+        href={link}
+        class="
+            {baseClasses}
+            {classList}
+    ">
+        <slot/>
+    </a>
+{:else}
+    <button
+        class="
+            {baseClasses}
+            {classList}
+        "
+        on:click={onClick}
+        disabled={disabled}
+    >
+        <slot/>     
+    </button>
+{/if}
