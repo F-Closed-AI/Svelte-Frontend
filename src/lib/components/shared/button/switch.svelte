0fe81d0a35da5e2b5a9b15e93cb3508d33d83c9a
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:56:05 +0100

    Added switch component

---
+++ src/lib/components/shared/button/switch.svelte
@@ -0,0 +1,23 @@
+<script lang=ts>
+    export let classList: string = "";
+    export let active: boolean = false;
+    export let onClick: (event: Event) => void = () => {};
+</script>
+
+<button
+    class="
+        px-3
+        py-2
+        border-b-2 
+        transition-colors
+        cursor-pointer
+        group
+        hover:border-light-btn-primary-background
+        {active ? "border-light-btn-primary-background" : "border-light-text-secondary"}
+        {classList}
+    "
+    on:click={onClick}
+    disabled={active}
+>
+    <slot></slot>
+</button>
