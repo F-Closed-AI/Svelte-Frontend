dd7f0609cba1d06d12686f85d380333adb5dc4af
2ndZorua <485892@student.fontys.nl>
2023-12-11 10:01:45 +0100

    Added edit character page

---
+++ src/routes/library/edit/[characterId]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang=ts>
+	import { PageCreate } from "$lib/components";
+    import type { PageCreate as Type } from "$lib/types";
+
+    export let data: Type;
+</script>
+
+<PageCreate 
+    {data} 
+    state="edit" 
+></PageCreate>
