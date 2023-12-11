b1594a177252dd25fce6f801cda301f0f4ad3a9e
2ndZorua <485892@student.fontys.nl>
2023-12-11 10:01:21 +0100

    Added server side rendering for the edit character page

---
+++ src/routes/library/edit/[characterId]/+page.server.ts
@@ -0,0 +1,55 @@
+import { fail, redirect } from "@sveltejs/kit";
+import { sendRequest } from "$lib/functions/api/request.js";
+import { schemaCreateCharacter, schemaSaveCharacter } from "$lib/schemas";
+import { superValidate } from "sveltekit-superforms/client";
+import type { Character } from "$lib/types";
+import type { PageServerLoad } from "./$types.js";
+
+export const load: PageServerLoad = async (event) => {
+    const charId = event.params.characterId;
+
+    try {
+        const character: Character = await sendRequest(`Character/${charId}`, "GET");
+        const versions: Character[] = await sendRequest(`Character/Characters/${character.charId}`, "GET");
+
+        return {
+            character,
+            versions
+        }
+    } catch {
+        // Show error modal: Character not found
+        throw redirect(302, "/library");
+    }
+
+}
+
+export const actions = {
+    create: async (event) => {
+        const form = await superValidate(event, schemaCreateCharacter);
+        
+        if (!form.valid) {
+            return fail(400, { form });
+        } 
+
+        return { form }
+    },
+    
+    save: async (event) => {
+        const form = await superValidate(event, schemaSaveCharacter);
+        console.log(form.data.charId);
+
+        if (form.data.charId === null) return fail(400, { form });
+
+        const character: Character = {
+            charId: form.data.charId,
+            userId: 17,
+            name: form.data.name,
+            age: Number(form.data.age),
+            backStory: form.data.backStory
+        }
+
+        await sendRequest(`Character/Update`, "PUT", character);
+
+        throw redirect(302, "/library");
+    }
+}
