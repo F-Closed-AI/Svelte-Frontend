12324367c5a137eee533e59f81aa7b1d19524a4c
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:57:04 +0100

    Renamed switcher component

---
+++ src/lib/components/shared/other/switcher.svelte
@@ -0,0 +1,39 @@
+<script lang=ts>
+    import { createEventDispatcher } from "svelte";
+	import { ButtonSwitch, TextSmall } from "$lib/components";
+
+    export let tabs: string[] = [];
+    export let activeTab: number = 0;
+
+    const dispatch = createEventDispatcher();
+
+    function onTabClick(index: number) {
+        activeTab = index;
+        dispatch("tabChange", { activeTab });
+    }
+</script>
+
+
+<div 
+    class="
+        flex 
+        gap-5
+">
+    {#each tabs as tab, index}
+        <ButtonSwitch 
+            active={activeTab === index}
+            onClick={() => onTabClick(index)}
+        >
+            <TextSmall
+                classList="
+                    !font-semibold
+                    text-light-text-secondary
+                    transition-colors
+                    group-hover:text-light-btn-primary-background 
+                    {activeTab === index && "!text-light-btn-primary-background"}
+            ">
+                {tab}
+            </TextSmall>
+        </ButtonSwitch>
+    {/each}
+</div>
