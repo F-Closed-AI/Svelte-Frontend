a34768623fe12d8ee8a95b930431bce551ef41f4
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:57:10 +0100

    Added table row component

---
+++ src/lib/components/shared/other/table/row.svelte
@@ -0,0 +1,11 @@
+<script lang=ts>
+    export let classList: string = "";
+    export let onClick: (event: Event) => void = () => {};    
+</script>
+
+<tr 
+    class={classList}
+    on:click={onClick}
+>
+    <slot></slot>
+</tr>
