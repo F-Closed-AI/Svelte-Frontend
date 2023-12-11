ae5f0be2afb052b15b7f835761b763ade464d254
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:56:37 +0100

    Added actionbar component

---
+++ src/lib/components/shared/other/actionbar.svelte
@@ -0,0 +1,14 @@
+<div class="
+    flex
+    gap-5
+    fixed 
+    bg-light-background 
+    bg-opacity-75
+    p-5
+    rounded-xl
+    bottom-10
+    z-10
+    sm:hidden
+">
+    <slot></slot>
+</div>
