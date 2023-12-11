1ce72039667ea3d98054bbd730c049622edf2448
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:55:40 +0100

    Added tab navigation component

--- src/lib/components/layout/tabNavigation.svelte
+++ src/lib/components/layout/tabNavigation.svelte
@@ -1,3 +1,48 @@
+<script lang=ts>
+	import { afterNavigate, goto } from "$app/navigation";
+    import { tabs } from "$lib/stores/tabNavigationStore";
+	import { fly, type FlyParams } from "svelte/transition";
+
+    let showContent: boolean = true;
+
+    const positions: string[] = [
+        "-translate-x-14 translate-y-4",
+        "-translate-y-4",
+        "translate-x-14 translate-y-4"
+    ];
+
+    const transitions: FlyParams[] = [
+        {
+            delay: 0, 
+            duration: 300, 
+            x: 60, 
+            y: 30
+        },
+        {
+            delay: 150, 
+            duration: 300, 
+            x: 0, 
+            y: 30
+        },
+        {
+            delay: 300, 
+            duration: 300, 
+            x: -60, 
+            y: 30
+        }
+    ]
+
+    function handleClick(link: string | undefined, onClick: (() => void) | undefined) {
+        if (link) {
+            goto(link);
+            return;
+        } 
+        onClick && onClick();
+    }
+
+    afterNavigate(() => showContent = true);
+</script>
+
 <div class="
     fixed
     hidden
@@ -19,32 +64,61 @@
         flex
         w-full
         justify-between
-        max-w-[10rem]
         py-6
     ">
         <slot name="left"></slot>
     </div>
-    <a 
-        href="/" 
-        class="
-            grid
-            place-content-center
-            absolute
-            text-light-text-tertiary
-            gradient
-            rounded-full
-            -top-5
-            px-3.5
-            py-3
-            cursor-pointer
-    ">
-        <slot name="icon"></slot>
-    </a>
+    <div class="absolute pr-12 top-0">
+        <button 
+            class="
+                grid
+                place-content-center
+                absolute
+                text-light-text-tertiary
+                gradient
+                rounded-full
+                -top-5
+                px-[0.85rem]
+                py-3
+                cursor-pointer
+                transition-transform
+                {showContent && $tabs.length > 0 && "rotate-45"}
+        "
+            on:click={() => showContent = !showContent}
+        >
+            <slot name="icon"></slot>
+        </button>
+        {#if showContent && $tabs.length > 0}
+            {#each $tabs as { icon, link, onClick, disabled }, index}
+                <button 
+                    class="
+                        grid
+                        content-center                        
+                        absolute 
+                        -top-[4rem] 
+                        {positions[index]}
+                        {disabled && "cursor-not-allowed"}
+                        bg-light-btn-primary-background
+                        py-3
+                        px-3
+                        rounded-full
+                        transition-colors
+                        hover:bg-light-btn-primary-hover
+                    " 
+                    transition:fly|external={transitions[index]}
+                    on:click|preventDefault={() => handleClick(link, onClick)}
+                    disabled={disabled}
+                >
+
+                    <i class="{icon} text-light-text-tertiary pointer-events-none"></i>
+                </button>
+            {/each}
+        {/if}
+    </div>
     <div class="
         flex
         w-full
         justify-between
-        max-w-[10rem]
         py-6
     ">
         <slot name="right"></slot>
