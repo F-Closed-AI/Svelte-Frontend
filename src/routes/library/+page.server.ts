62ac82f56c605e242603d0f084a0a29deb6016ab
2ndZorua <485892@student.fontys.nl>
2023-12-11 10:00:02 +0100

    Updated server side rendering for library page

--- src/routes/library/+page.server.ts
+++ src/routes/library/+page.server.ts
@@ -1,15 +1,15 @@
 import { sendRequest } from "$lib/functions";
 import type { PageServerLoad } from "./$types";
-import type { Character } from "$lib/types/types";
+import type { Character } from "$lib/types";
 
 export const load: PageServerLoad = async () => {
     let characters: Character[];
     try {
-        characters = await sendRequest(`Character/GetCharactersByUserId?userId=17`, "GET");
-    } catch {
+        characters = await sendRequest(`Character/User/17`, "GET");
+    } 
+    catch {
         characters = [];
     }
-
     return {
         characters
     }
