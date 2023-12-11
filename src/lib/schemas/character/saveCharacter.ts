c391afbc70147bbfdf27776e747d7d27538fec8d
2ndZorua <485892@student.fontys.nl>
2023-12-11 09:58:33 +0100

    Updated save character schema

--- src/lib/schemas/character/saveCharacter.ts
+++ src/lib/schemas/character/saveCharacter.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const schemaSaveCharacter = z.object({
+    charId: z.string().nullable(),
     name: z.string(),
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     age: z.number().default(null!),
