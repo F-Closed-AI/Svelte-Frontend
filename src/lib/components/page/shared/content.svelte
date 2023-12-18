de851d1fc0f8a90adb3fca8ba78eb8bfbf813229
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:51:41 +0100

    Added the content component

---
+++ src/lib/components/page/shared/content.svelte
@@ -0,0 +1,19 @@
+<div 
+    class="
+        flex
+        flex-col
+        gap-5
+        h-max
+        pb-10
+    "
+>
+    <slot name="title"></slot>
+    <div 
+        class="
+            flex
+            flex-col
+            gap-10
+    ">
+        <slot name="content"></slot>
+    </div>
+</div>
