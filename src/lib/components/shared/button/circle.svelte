906ddec6c9ab9dfb0ea8ebcb2066a2efea56bd1d
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:51:58 +0100

    Updated the circle button component

--- src/lib/components/shared/button/circle.svelte
+++ src/lib/components/shared/button/circle.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
+	import { Tooltip, TextSmall } from "../../";
+
     export let link: string = "";    
     export let classList: string = "";
     export let disabled: boolean = false;
-    export let onClick: (event: Event) => void = () => {};    
+    export let tooltip: string = "";
+    export let onClick: (event: Event) => void = () => {};   
 
-    let baseClasses: string = "flex text-light-text-primary bg-light-background px-3 py-2 rounded-full cursor-pointer";
+    let showTooltip: boolean = false;
+    let baseClasses: string = "relative flex text-light-text-primary bg-light-background px-3 py-2 rounded-full cursor-pointer";
+
+    function onMouseEvent(show: boolean) {
+        if (tooltip === "") return;
+        showTooltip = show;
+    }
 </script>
 
 {#if link != ""}
@@ -23,8 +32,13 @@
             {classList}
         "
         on:click={onClick}
+        on:mouseenter={() => onMouseEvent(true)}
+        on:mouseleave={() => onMouseEvent(false)}
         disabled={disabled}
     >
-        <slot/>     
+        <slot/> 
+        <Tooltip shown={showTooltip} side="up">
+            <TextSmall classList="text-light-text-tertiary">{tooltip}</TextSmall>
+        </Tooltip>    
     </button>
 {/if}
