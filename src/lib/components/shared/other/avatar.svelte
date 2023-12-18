3131706ba6fdc29484740a10578e401ef6f8e668
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:52:34 +0100

    Updated the avatar component

---
+++ src/lib/components/shared/other/avatar.svelte
@@ -0,0 +1,17 @@
+<script lang=ts>
+    export let classList: string = ""
+    export let image: string = "";
+    export let alt: string = "";
+</script>
+
+<img 
+    class="
+        h-20 
+        w-20 
+        object-cover 
+        rounded-full
+        {classList}
+    " 
+    src={image} 
+    alt={alt}
+>
