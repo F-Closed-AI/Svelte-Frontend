2ca30f88fd8c3a0199d0ccef15648fcef511c822
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:58:10 +0100

    Added the edit room page

---
+++ src/routes/rooms/edit/[roomId]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang=ts>
+	import { PageCreateRoom } from "$lib/components";
+	import type { PageCreateRoom as Type } from "$lib/types";
+
+	export let data: Type;
+</script>
+
+<PageCreateRoom 
+    {data}
+    state="edit"
+></PageCreateRoom>
