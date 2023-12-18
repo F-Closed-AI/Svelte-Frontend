3cc3a12bd194079b30d37b2f41a31454ab12858b
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:57:18 +0100

    Added the create room page

---
+++ src/routes/rooms/create/+page.svelte
@@ -0,0 +1,8 @@
+<script lang=ts>
+	import { PageCreateRoom } from "$lib/components";
+	import type { PageCreateRoom as Type } from "$lib/types";
+
+	export let data: Type;
+</script>
+
+<PageCreateRoom {data}></PageCreateRoom>
