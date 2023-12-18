4bfbf4cb9e5caf91cd63ac6d50403df1fd1b3276
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:55:22 +0100

    Updated the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang=ts>
-    import { TextDecoration, TextLarge, Slider, TextSmall, TextDetail, CardNew, CardBanner, CardCharacter, CardPreview, ButtonPrimary } from "$lib/components";
+    import { TextDecoration, TextLarge, Slider, TextSmall, TextDetail, CardNew, CardBanner, CardCharacter, CardPreview, ButtonPrimary, ButtonCircle } from "$lib/components";
+    import { animate, inView } from "motion";
     import generated_1 from "$lib/img/generated_1.png";
     import generated_2 from "$lib/img/generated_2.png";
     import generated_3 from "$lib/img/generated_3.png";
@@ -7,8 +8,19 @@
     import generated_5 from "$lib/img/generated_5.png";
     import generated_6 from "$lib/img/generated_6.png";
     import generated_7 from "$lib/img/generated_7.png";
+	import { onMount } from "svelte";
 
+    let scroll: HTMLDivElement;
+    let sliderContainer: HTMLDivElement;
+    let banner: HTMLDivElement;
     const images = [generated_1, generated_2, generated_3, generated_4, generated_5, generated_6, generated_7]
+
+    onMount(() => {
+        inView(banner, () => {
+            animate(scroll, { opacity: [1] });
+            return () => animate(scroll, { opacity: [1, 0]});
+        });
+    })
 </script>
 
 <div class="
@@ -16,38 +28,75 @@
     gap-20
     isolate
 ">
-    <div class="
-        flex 
-        flex-col 
-        gap-12
-        flex-[2.25] 
-        max-w-[60rem]
-        -mt-2.5
-        xxxl:max-w-none
-    ">
-        <CardBanner></CardBanner>
-        {#each ["Popular", "New", "Most Liked"] as category}
-            <div class="
-                flex 
-                flex-col
-                justify-between
-                gap-6
-            ">
+    <div class="relative">
+        <div class="
+            flex 
+            flex-col 
+            gap-12
+            flex-[2.25] 
+            max-w-[60rem]
+            -mt-2.5
+            xxxl:max-w-none
+        ">
+            <div bind:this={banner}>
+                <CardBanner></CardBanner>
+            </div>
+            {#each ["Popular", "New", "Most Liked"] as category, index}
                 <div class="
                     flex 
+                    flex-col
                     justify-between
-                    xs:flex-col
+                    gap-6
                 ">
-                    <TextLarge>{category} Characters</TextLarge>
-                    <TextDecoration classList="text-light-text-primary" line={false} link="/">View all</TextDecoration>
+                    <div class="
+                        flex 
+                        justify-between
+                        xs:flex-col
+                    ">
+                        <TextLarge>{category} Characters</TextLarge>
+                        <TextDecoration classList="text-light-text-primary" line={false} link="/">View all</TextDecoration>
+                    </div>
+                    {#if index === 2}
+                        <div bind:this={sliderContainer}>   
+                            <Slider>
+                                {#each images as image}
+                                    <CardCharacter classList="snap-start" img={image}></CardCharacter>
+                                {/each}
+                            </Slider>
+                        </div>
+                    {:else}
+                        <Slider>
+                            {#each images as image}
+                                <CardCharacter classList="snap-start" img={image}></CardCharacter>
+                            {/each}
+                        </Slider>
+                    {/if}
                 </div>
-                <Slider>
-                    {#each images as image}
-                        <CardCharacter classList="snap-start" img={image}></CardCharacter>
-                    {/each}
-                </Slider>
+            {/each}
+            <div 
+                class="
+                    sticky
+                    ml-[50%]
+                    bottom-0
+                    translate-y-5
+                    sm:hidden
+                "
+                bind:this={scroll}
+            >
+                <ButtonCircle 
+                    classList="
+                        px-3.5 
+                        py-3 
+                        bg-light-btn-primary-background 
+                        transition-colors
+                        hover:bg-opacity-90
+                    "
+                    onClick={() => sliderContainer.scrollIntoView({ behavior: "smooth", block: "nearest" })}
+                >
+                    <i class="fa-solid fa-angles-down text-light-text-tertiary"></i>
+                </ButtonCircle>
             </div>
-        {/each}
+        </div>
     </div>
     <div class="
         flex-1
