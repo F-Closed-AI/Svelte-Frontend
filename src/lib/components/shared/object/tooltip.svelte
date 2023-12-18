bb3dbf2fb5ab2afd812a2a69f54d7392f7aa3201
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:52:27 +0100

    Updated the tooltip component

--- src/lib/components/shared/object/tooltip.svelte
+++ src/lib/components/shared/object/tooltip.svelte
@@ -1,9 +1,14 @@
 <script lang=ts>
 	import { scale } from "svelte/transition";
 
+    export let format: "short" | "default" = "default";
     export let side: "left" | "right" | "up" | "down" = "right";
+    export let gap: string = 4.25;
     export let shown: boolean = false;
     export let classList: string = "";
+
+    let defaultClassList = "py-4 px-5 before:content-[''] before:absolute before:flex before:items-center before:bg-inherit before:clip-triangle before:w-4 before:h-4";
+    let shortClassList = "py-3 px-4";
 </script>
 
 {#if shown}
@@ -15,24 +20,17 @@
             bg-light-btn-primary-background
             w-max
             h-full
-            py-4
-            px-5
             rounded-lg
             pointer-events-none
-            before:content-['']
-            before:absolute
-            before:flex
-            before:items-center
-            before:bg-inherit
-            before:clip-triangle
-            before:w-4
-            before:h-4
-            {side === "left" && "right-[4.25rem] before:-right-2 before:rotate-90"}
-            {side === "right" && "left-[4.25rem] before:-left-2 before:-rotate-90"}
-            {side === "up" && "-top-[3.5rem] before:-bottom-2 before:-translate-x-[50%] before:left-[50%] before:rotate-180"}
-            {side === "down" && "-bottom-[3.5rem] before:-top-2 before:-translate-x-[50%] before:left-[50%]"}
+            {format === "default" && defaultClassList}
+            {format === "short" && shortClassList}
+            {side === "left" && `right-[var(--gap)] before:-right-2 before:rotate-90`}
+            {side === "right" && `left-[var(--gap)] before:-left-2 before:-rotate-90`}
+            {side === "up" && `-top-[var(--gap)] before:-bottom-2 before:-translate-x-[50%] before:left-[50%] before:rotate-180`}
+            {side === "down" && `-bottom-[var(--gap)] before:-top-2 before:-translate-x-[50%] before:left-[50%]`}
             {classList}
         "
+        style="--gap: {gap}rem;"
         transition:scale
     >
         <slot/>
