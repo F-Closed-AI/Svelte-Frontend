54d2fd85238d4a689ce0b10ab90f8a94d9d6e849
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:53:17 +0100

    Updated the decoration text component

--- src/lib/components/shared/text/decoration.svelte
+++ src/lib/components/shared/text/decoration.svelte
@@ -3,6 +3,7 @@
     export let classList: string = "";
     export let line: boolean = true;
     export let link: string = "";
+    export let onClick: (event: Event) => void = () => {};   
 
     let baseClasses: string = `flex items-center gap-3 font-secondary font-normal text-base text-light-background max-w-max uppercase ${link && "link"} ${line && "before:inline-block before:content-[''] before:bg-light-background before:w-14 before:h-line"}`
 </script>
@@ -18,12 +19,14 @@
         <slot></slot>
     </a>
 {:else}
-    <span 
+    <button 
         class:skeleton={skeleton} 
         class="   
             {baseClasses}
             {classList}
-    ">
+        "
+        on:click={onClick}
+    >
         <slot></slot>
-    </span>
+    </button>
 {/if}
