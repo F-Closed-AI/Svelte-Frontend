7a0368e1c0a00f1e2ae84db49d94f191fce090de
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:52:18 +0100

    Added the modal component

---
+++ src/lib/components/shared/object/modal.svelte
@@ -0,0 +1,31 @@
+<script lang=ts>
+    export let showModal: boolean = false;
+
+    let dialog: HTMLDialogElement;
+
+    $: if (dialog && showModal) dialog.showModal();
+</script>
+
+{#if showModal}
+    <div 
+        class="
+            fixed
+            flex
+            items-center
+            justify-center
+            w-screen
+            h-screen
+            bg-black
+            bg-opacity-50
+            top-0
+            left-0
+            z-50
+        "
+        on:click|self={() => showModal = false}
+        on:keypress={() => showModal = false}
+    >
+        <div class="bg-light-dashboard-light w-max h-max p-10 rounded-lg sm:w-full sm:h-full">
+            <svelte:component on:click this={$$restProps.content} {...$$restProps}/>
+        </div>
+    </div>
+{/if}
