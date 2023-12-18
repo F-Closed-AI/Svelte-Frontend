3b86bf21681bd4a9c958d85f0775326ec1cb3c0c
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:55:08 +0100

    Updated the entity types

--- src/lib/types/entities/types.ts
+++ src/lib/types/entities/types.ts
@@ -8,10 +8,25 @@ export interface Character {
     id?: string,
     charId?: string,
     userId?: number,
+    prompt: string,
     name: string,
     age: number,
     backStory: string,
     dateTime?: string
+}
+
+export interface Room {
+    id?: string,
+    roomId?: string,
+    name: string,
+    userId: number,
+    charId?: string[],
+    dateTime?: string
+}
+
+export interface Label {
+    name: string,
+    color: string
 }
 
 export interface RemainingTime {
