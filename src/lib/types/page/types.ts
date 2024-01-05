f85d25eebff3cbb781bd41f3b0bc9212d3141bf9
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:06:24 +0100

    Updated the page types

--- src/lib/types/page/types.ts
+++ src/lib/types/page/types.ts
@@ -1,18 +1,40 @@
 import type { SuperValidated } from "sveltekit-superforms";
-import type { SchemaCreateCharacter, SchemaCreateRoom, SchemaSaveCharacter } from "$lib/schemas";
-import type { Character, Room } from "../entities/types";
+import type { SchemaCreateCharacter, SchemaCreateConversation, SchemaCreateLabel, SchemaCreateRoom, SchemaDeleteRoom, SchemaSaveCharacter } from "$lib/schemas";
+import type { Character, Label, Room } from "../entities/types";
+import type { schemaDeleteCharacter } from "$lib/schemas/character/deleteCharacter";
+import type { SchemaDeleteLabel } from "$lib/schemas/label/deleteLabel";
 
 export interface PageCreateCharacter {
     createCharacterForm: SuperValidated<SchemaCreateCharacter>,
     saveCharacterForm: SuperValidated<SchemaSaveCharacter>,
+    deleteCharacterForm: SuperValidated<schemaDeleteCharacter>,
     character: Character,
     versions: Character[]
 }
 
+export interface PageViewCharacter {
+    character: Character
+}
+
 export interface PageCreateRoom {
     createRoomForm: SuperValidated<SchemaCreateRoom>,
+    deleteRoomForm: SuperValidated<SchemaDeleteRoom>,
     allCharacters: Character[],
     currentCharacters: Character[],
+    allLabels: Label[],
+    currentLabels: Label[],
     room: Room,
     versions: Room[]
+}
+
+export interface PageViewRoom {
+    createConversationForm: SuperValidated<SchemaCreateConversation>,
+    currentCharacters: Character[],
+    room: Room,
+}
+
+export interface PageCreateLabel {
+    createLabelForm: SuperValidated<SchemaCreateLabel>,
+    deleteLabelForm: SuperValidated<SchemaDeleteLabel>,
+    label: Label
 }
