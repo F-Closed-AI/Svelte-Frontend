96922c47ab25934e0c4e13c8898a9e137f5b6cea
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:04:49 +0100

    Updated the get random image function

--- src/lib/functions/other/image.ts
+++ src/lib/functions/other/image.ts
@@ -2,18 +2,15 @@ import generated_1 from "$lib/img/generated_1.png";
 import generated_2 from "$lib/img/generated_2.png";
 import generated_3 from "$lib/img/generated_3.png";
 import generated_4 from "$lib/img/generated_4.png";
-import generated_5 from "$lib/img/generated_5.png";
-import generated_6 from "$lib/img/generated_6.png";
-import generated_7 from "$lib/img/generated_7.png";
+// import generated_5 from "$lib/img/generated_5.png";
+// // import generated_6 from "$lib/img/generated_6.png";
+// import generated_5 from "$lib/img/generated_7.png";
 
 const images = [
     generated_1,
     generated_2,
     generated_3,
-    generated_4,
-    generated_5,
-    generated_6,
-    generated_7
+    generated_4
 ]
 
 export function getRandomImage() {
