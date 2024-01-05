3dc5591bf75c8939912116d957a2e517d733929f
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:05:53 +0100

    Updated  the create room schema

--- src/lib/schemas/room/createRoom.ts
+++ src/lib/schemas/room/createRoom.ts
@@ -4,12 +4,7 @@ export const schemaCreateRoom = z.object({
     roomId: z.string().nullable(),
     name: z.string().min(5).max(20),
     characterIds: z.array(z.string()),
-    labels: z.array(
-        z.object({
-            name: z.string(),
-            color: z.string()
-        })
-    )
+    labelIds: z.array(z.string())
 });
 
 export type SchemaCreateRoom = typeof schemaCreateRoom;
