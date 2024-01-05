96c1e7e81d994cb4e5f834057d3e89f678891d6d
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:08:55 +0100

    Updated the server layout file for the rooms page

--- src/routes/rooms/+layout.server.ts
+++ src/routes/rooms/+layout.server.ts
@@ -1,13 +1,19 @@
-import { schemaCreateRoom } from "$lib/schemas";
+import { schemaCreateRoom, schemaDeleteRoom } from "$lib/schemas";
 import { superValidate } from "sveltekit-superforms/client";
 import type { PageServerLoad } from "./$types";
-import { sendRequest } from "$lib/functions";
+import { sendRequests } from "$lib/functions";
 
 export const load: PageServerLoad = async (event) => {
     const createRoomForm = await superValidate(event, schemaCreateRoom);
-    const allCharacters = await sendRequest(`Character/User/17`, "GET");
+    const deleteRoomForm = await superValidate(event, schemaDeleteRoom);
+    const [allCharacters, allLabels] = await sendRequests([
+        { url: `Character/User/17`, method: "GET" },
+        { url: `Label/User/17`, method: "GET" }
+    ]);
     return {
         createRoomForm,
-        allCharacters
+        deleteRoomForm,
+        allCharacters,
+        allLabels
     }
 }
