e03269fb770e5cbe750bd093c008bb3a52a721fe
2ndZorua <485892@student.fontys.nl>
2024-01-05 13:54:11 +0100

    Added the sidebar item nested component

---
+++ src/lib/components/layout/sidebarItemNested.svelte
@@ -0,0 +1,35 @@
+<script lang=ts>
+    export let text: string = "";
+    export let link: string = "";
+</script>
+
+<a 
+    href={link} 
+    class="
+        flex
+        gap-2
+        px-5
+        md:flex-col
+    "
+    data-sveltekit-preload-data
+>
+    <i class="fa-solid fa-chevron-down rotate-45 md:hidden"></i>
+    <i class="fa-solid fa-minus hidden rotate-90 md:block"></i>
+    <div 
+        class="
+            md:hidden
+        "
+    >
+        <slot name="text"></slot>
+    </div>
+    <button
+        class="
+            relative
+            hidden
+            md:block
+        "
+        title={text} 
+    >
+        <slot name="icon"></slot>
+    </button>
+</a>
