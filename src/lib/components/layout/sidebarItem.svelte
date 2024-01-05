c8b767b7bdb27db48f3acaccaabaa54237d45390
2ndZorua <485892@student.fontys.nl>
2024-01-05 13:54:04 +0100

    Updated the sidebar item component

--- src/lib/components/layout/sidebarItem.svelte
+++ src/lib/components/layout/sidebarItem.svelte
@@ -1,5 +1,6 @@
 <script lang=ts>
     import { TextSmall, Tooltip } from "$lib/components/index.ts";
+	import { slide } from "svelte/transition";
 
     export let text: string = "default tooltip";
     export let link: string = "";
@@ -8,44 +9,55 @@
     let showTooltip: boolean = false;
 </script>
 
-<a 
-    href={link}
-    class="
-        flex
-        items-center
-        gap-5
-        text-light-text-secondary
-        bg-light-btn-primary-color
-        px-5
-        py-3
-        rounded-lg
-        transition-colors
-        duration-300
-        group
-        hover:bg-light-btn-primary-background
-        hover:text-light-btn-primary-color
-        {active && "!bg-light-btn-primary-background !text-light-btn-primary-color"}
-    "
-    on:mouseenter={() => showTooltip = true}
-    on:mouseleave={() => showTooltip = false}
->
-    <div class="
-            flex 
-            justify-center
+<div>
+    <a 
+        href={link}
+        class="
+            flex
             items-center
-            relative
-            w-8
-
+            gap-5
+            text-light-text-secondary
+            bg-light-btn-primary-color
+            px-5
+            py-3
+            rounded-lg
+            transition-colors
+            duration-300
+            group
+            hover:bg-light-btn-primary-background
+            hover:text-light-btn-primary-color
+            {active && "!bg-light-btn-primary-background !text-light-btn-primary-color"}
         "
+        on:mouseenter={() => showTooltip = true}
+        on:mouseleave={() => showTooltip = false}
+        data-sveltekit-preload-data
     >
-        <slot name="icon"></slot>
-        <Tooltip shown={showTooltip} classList={"hidden md:flex"}>
-            <TextSmall>{text}</TextSmall>
-        </Tooltip>
-    </div>
-    <div class="
-        md:hidden
-    ">
-        <slot name="text"></slot>
-    </div>
-</a>
+        <div 
+            class="
+                flex 
+                justify-center
+                items-center
+                relative
+                w-8
+
+            "
+        >
+            <slot name="icon"></slot>
+            <Tooltip shown={showTooltip} classList={"hidden md:flex"}>
+                <TextSmall>{text}</TextSmall>
+            </Tooltip>
+        </div>
+        <div 
+            class="
+                md:hidden
+            "
+        >
+            <slot name="text"></slot>
+        </div>
+    </a>
+    {#if active}
+        <div class="absolute w-full flex md:justify-center" transition:slide>
+            <slot name="items"></slot>
+        </div>
+    {/if}
+</div>
