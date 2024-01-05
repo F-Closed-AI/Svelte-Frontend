1a2a32ff109f6114dfdf1309af7fdbcca5d3cd39
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:04:56 +0100

    Updated the schemas barrel file

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -5,5 +5,12 @@
 export { schemaCreateCharacter, type SchemaCreateCharacter } from "./character/createCharacter"; 
 export { schemaSaveCharacter, type SchemaSaveCharacter } from "./character/saveCharacter";
 
+//Conversation
+export { schemaCreateConversation, type SchemaCreateConversation } from "./conversation/createConversation";
+
+// Label
+export { schemaCreateLabel, type SchemaCreateLabel } from "./label/createLabel";
+
 // Room
 export { schemaCreateRoom, type SchemaCreateRoom } from "./room/createRoom";
+export { schemaDeleteRoom, type SchemaDeleteRoom } from "./room/deleteRoom";
