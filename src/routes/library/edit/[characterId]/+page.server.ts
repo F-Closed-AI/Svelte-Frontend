dee0516e46daf99606addd4255c289ff9c517db5
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:08:41 +0100

    Updated the server side rendering for the edit character page

--- src/routes/library/edit/[characterId]/+page.server.ts
+++ src/routes/library/edit/[characterId]/+page.server.ts
@@ -1,9 +1,11 @@
-import { fail, redirect } from "@sveltejs/kit";
+import { fail } from "@sveltejs/kit";
+import { redirect, setFlash } from "sveltekit-flash-message/server";
 import { sendRequest } from "$lib/functions/api/request.js";
 import { schemaCreateCharacter, schemaSaveCharacter } from "$lib/schemas";
 import { superValidate } from "sveltekit-superforms/client";
 import type { Character } from "$lib/types";
 import type { PageServerLoad } from "./$types.js";
+import { schemaDeleteCharacter } from "$lib/schemas/character/deleteCharacter.js";
 
 export const load: PageServerLoad = async (event) => {
     const charId = event.params.characterId;
@@ -17,10 +19,8 @@ export const load: PageServerLoad = async (event) => {
             versions
         }
     } catch {
-        // Show error modal: Character not found
-        throw redirect(302, "/library");
+        throw redirect("/library", { type: "error", message: "Character not found." }, event);
     }
-
 }
 
 export const actions = {
@@ -37,7 +37,28 @@ export const actions = {
     save: async (event) => {
         const form = await superValidate(event, schemaSaveCharacter);
 
+        let noChanges = false;
+
         if (form.data.charId === null) return fail(400, { form });
+
+        try {
+            const response: Character = await sendRequest(`Character/${event.params.characterId}`, "GET");
+
+            if (
+                response.prompt === form.data.prompt &&
+                response.name === form.data.name &&
+                response.age === form.data.age &&
+                response.backStory === form.data.backStory
+            ) {
+                setFlash({ type: 'error', message: 'No changes made.' }, event);
+                noChanges = true;
+            }
+        } catch {
+            setFlash({ type: 'error', message: 'Internal error occured whilst saving character.' }, event);
+            noChanges = true;
+        }
+
+        if (noChanges) return fail(400, { form });
 
         const character: Character = {
             charId: form.data.charId,
@@ -49,7 +70,15 @@ export const actions = {
         }
 
         await sendRequest(`Character/Update`, "PUT", character);
+        throw redirect("/library", { type: "success", message: "Succesfully pdated character." }, event);
+    },
 
-        throw redirect(302, "/library");
+    delete: async(event) => {
+        const form = await superValidate(event, schemaDeleteCharacter);
+
+        if (!form.valid) return fail(400, { form });
+
+        await sendRequest("Character/Delete", "DELETE", form.data.charId);;
+        throw redirect("/library", { type: "success", message: "Succesfully deleted character." }, event);
     }
 }
