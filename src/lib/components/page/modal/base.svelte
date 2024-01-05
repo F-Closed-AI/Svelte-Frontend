b93363a0c41a0601d8d31959e2f648e557676c78
2ndZorua <485892@student.fontys.nl>
2024-01-05 13:55:08 +0100

    Added the base modal component

---
+++ src/lib/components/page/modal/base.svelte
@@ -0,0 +1,31 @@
+<script lang=ts>
+	import { TextLarge, Line } from "$lib/components";
+
+    export let classList: string = "";
+    export let name: string = "";
+    export let createLink: string = "";
+    export let onClose: (event: Event) => void = () => {};
+</script>
+
+<div class="flex flex-col gap-10 w-[40rem] sm:w-full {classList}">
+    <div class="flex flex-col gap-5">
+        <div class="flex justify-between">
+            <div class="flex gap-5">
+                <TextLarge classList="whitespace-nowrap">{name}</TextLarge>
+                {#if createLink !== ""}
+                    <a 
+                        href={createLink}
+                        class="icon flex items-center" 
+                    >
+                        <i class="fa-solid fa-plus"></i>
+                    </a>
+                {/if}
+            </div>
+            <button class="flex items-center" title="Close" on:click={onClose}>
+                <i class="fa-solid fa-xmark icon"></i>
+            </button>
+        </div>
+        <Line></Line>
+    </div>
+    <slot name="content"></slot>
+</div>
