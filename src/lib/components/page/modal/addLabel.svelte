7a1281092c47084b4ef2d2109c498029e006ae25
2ndZorua <485892@student.fontys.nl>
2024-01-05 13:55:02 +0100

    Updated the add label modal component

--- src/lib/components/page/modal/addLabel.svelte
+++ src/lib/components/page/modal/addLabel.svelte
@@ -1,31 +1,68 @@
 <script lang=ts>
-	import { TextLarge, Line, TextBase } from "$lib/components";
-    import type { Label } from "$lib/types";
+	import { TextBase, Label, ModalBase, TextSmall } from "$lib/components";
+    import type { Label as LabelType } from "$lib/types";
+	import { fade } from "svelte/transition";
 
-    export let currentLabels: Label[] = []
-    export let onAdd: () => void = () => {};
+    export let allLabels: LabelType[] = [];
+    export let currentLabels: LabelType[] = [];
+    export let onAdd: (label: LabelType) => void = () => {};
     export let onClose: (event: Event) => void = () => {};
 
-    function addLabel(label: Label) {
-        if (currentLabels.includes(label)) {
-            const index = currentLabels.indexOf(label);
-            currentLabels.splice(index, 1);
-        } else {
-            currentLabels.push(label);
-        }
-        onAdd();
+    $: currentLabelIds = currentLabels.map(l => l.id);
+
+    function addLabel(label: LabelType) {
+        onAdd(label);
+        currentLabels = currentLabels;
     }
 </script>
 
-<div class="flex flex-col gap-10">
-    <div class="flex flex-col gap-5">
-        <div class="flex justify-between w-[40rem] sm:w-full">
-            <TextLarge classList="whitespace-nowrap">Add Label</TextLarge>
-            <button class="flex items-center" on:click={onClose}>
-                <i class="fa-solid fa-xmark icon"></i>
-            </button>
-        </div>
-        <Line></Line>
+<ModalBase
+    name="Add Label"
+    createLink="/rooms/labels"
+    {onClose}
+>
+    <div slot="content">
+        {#if allLabels.length > 0}
+            <div class="flex flex-wrap gap-5">
+                {#each allLabels as label}
+                    <button 
+                        class="relative flex-1"
+                        on:click={() => addLabel(label)}
+                        title={label.name}
+                    >
+                        <Label classList="flex-1" color={label.color}>
+                            <TextSmall 
+                                classList="
+                                    !font-medium 
+                                    text-light-text-tertiary
+                            ">
+                                {label.name}
+                            </TextSmall>
+                        </Label>
+                        {#if currentLabelIds.includes(label.id)}
+                            <div 
+                                class="
+                                    flex
+                                    items-center
+                                    justify-center
+                                    absolute
+                                    w-full
+                                    h-full
+                                    rounded-full
+                                    bg-light-btn-primary-background
+                                    bg-opacity-80
+                                    top-0
+                                "
+                                transition:fade|local={{ duration: 250 }}
+                            >
+                                <i class="fa-solid fa-check text-light-text-tertiary"></i>
+                            </div>
+                        {/if}
+                    </button>
+                {/each}
+            </div>
+        {:else}
+            <TextBase>No Labels</TextBase>
+        {/if}
     </div>
-    <TextBase>No Labels</TextBase>
-</div>
+</ModalBase>
