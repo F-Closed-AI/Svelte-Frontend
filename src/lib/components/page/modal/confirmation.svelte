b66d742f6d5e1abef0cdb88dbc58b473ab200af0
2ndZorua <485892@student.fontys.nl>
2024-01-05 13:55:26 +0100

    Added the confirmation modal component

---
+++ src/lib/components/page/modal/confirmation.svelte
@@ -0,0 +1,27 @@
+<script lang=ts>
+    import { ButtonPrimary, ButtonSecondary, ModalBase, TextBase } from "$lib/components";
+
+    export let name: string = "";
+    export let type: string = "";
+    export let message: string = "";
+    export let onClose: (event: Event) => void = () => {};
+    export let onDelete: () => void = () => {};
+</script>
+
+<ModalBase
+    classList="sm:relative sm:h-full"
+    {name}
+    {onClose}
+>
+    <div slot="content" class="flex flex-col gap-10">
+        <TextBase>{message}</TextBase>
+        <div class="w-full flex justify-end gap-5 sm:absolute sm:bottom-0">
+            <ButtonPrimary onClick={onClose}>
+                <TextBase classList="!font-semibold">Cancel</TextBase>
+            </ButtonPrimary>
+            <ButtonSecondary onClick={onDelete}>
+                <TextBase classList="!font-semibold">Delete {type}</TextBase>
+            </ButtonSecondary>
+        </div>
+    </div>
+</ModalBase>
