2619dd5cb523cc434d7fb2e9cdffba55ad345f76
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:09:08 +0100

    Updated the server side rendering for the rooms page

--- src/routes/rooms/+page.server.ts
+++ src/routes/rooms/+page.server.ts
@@ -1,16 +1,14 @@
-import { sendRequest } from "$lib/functions";
+import { sendRequest, sendRequests } from "$lib/functions";
+import type { Label, Room } from "$lib/types";
 import type { PageServerLoad } from "./$types";
-import type { Room } from "$lib/types";
 
 export const load: PageServerLoad = async () => {
-    let rooms: Room[];
-    try {
-        rooms = await sendRequest(`Room/User/17`, "GET");
-    } 
-    catch {
-        rooms = [];
-    }
+    const rooms: Room[] = await sendRequest(`Room/User/17`, "GET");
+    const labelRequests = rooms.map(x => ({ url: `Room/Labels/${x.id}`, method: "GET" }));
+    const labels = await sendRequests(labelRequests) as Array<Label[]>;
+
     return {
-        rooms
+        rooms,
+        labels
     }
 }
