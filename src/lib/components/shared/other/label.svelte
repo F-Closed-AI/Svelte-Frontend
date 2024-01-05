1edba500d0d675295834b3a169cbd2e245a960f8
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:04:09 +0100

    Updated the label component

--- src/lib/components/shared/other/label.svelte
+++ src/lib/components/shared/other/label.svelte
@@ -1,12 +1,14 @@
 <script lang=ts>
+    export let classList: string = "";
     export let color: string = "#acacac";
 </script>
 
 <div 
     class="
         px-8 py-1
-        w-max
         rounded-full
+        whitespace-nowrap
+        {classList}
     "
     style="
         background-color: {color};
