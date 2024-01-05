27dce2a3dd78420fc63dc89b70bc260e4f589609
2ndZorua <485892@student.fontys.nl>
2024-01-05 15:06:42 +0100

    Added the edit label page

---
+++ src/routes/rooms/labels/edit/[labelId]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang=ts>
+	import { PageCreateLabel } from "$lib/components";
+	import type { PageCreateLabel as Type } from "$lib/types";
+
+	export let data: Type;
+</script>
+
+<PageCreateLabel 
+    {data}
+    state="edit"
+></PageCreateLabel>
