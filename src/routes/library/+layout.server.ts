32a9c7b82ffeb7dca9e66f554e6eb379144d32fb
2ndZorua <485892@student.fontys.nl>
2024-01-05 14:07:06 +0100

    Updated the server side rendering for the library page

--- src/routes/library/+layout.server.ts
+++ src/routes/library/+layout.server.ts
@@ -1,12 +1,15 @@
 import { schemaCreateCharacter, schemaSaveCharacter } from "$lib/schemas";
 import { superValidate } from "sveltekit-superforms/client";
 import type { PageServerLoad } from "./$types";
+import { schemaDeleteCharacter } from "$lib/schemas/character/deleteCharacter";
 
 export const load: PageServerLoad = async (event) => {
     const createCharacterForm = await superValidate(event, schemaCreateCharacter);
     const saveCharacterForm = await superValidate(event, schemaSaveCharacter);
+    const deleteCharacterForm = await superValidate(event, schemaDeleteCharacter);
     return {
         createCharacterForm,
-        saveCharacterForm
+        saveCharacterForm,
+        deleteCharacterForm
     }
 }
